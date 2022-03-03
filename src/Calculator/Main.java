9288499362430b09178f15b3752b6d568eafa9e4
TikhonB <tishka.bogdanov@gmail.com>
2022-03-03 22:56:33 +0300

    Add code and style formatting

--- src/Calculator/Main.java
+++ src/Calculator/Main.java
@@ -1,4 +1,22 @@
 package Calculator;
 
 public class Main {
+    public static void main(String[] args) {
+        Calculator calc = Calculator.instance.get();
+        try {
+            int a = calc.plus.apply(1, 2);
+            int b = calc.minus.apply(1, 1);
+            int c = calc.devide.apply(a, b); // блок try что-бы поймать исключения деления на 0
+            calc.println.accept(c);
+        } catch (ArithmeticException e) {
+            System.out.println("Деление на 0!");
+        }
+        int d = calc.multiply.apply(10, 20);
+        int f = calc.pow.apply(10);
+        int g = calc.abs.apply(-5);
+        boolean e = calc.isPositive.test(-5);
+        calc.println.accept(f);
+        System.out.println(g);
+        System.out.println(e);
+    }
 }
